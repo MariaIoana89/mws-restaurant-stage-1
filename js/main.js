abef1b5157e45c3538473ae82c6de966d5266eff
MariaIoana89 <maria_ioana28@yahoo.com>
2018-08-03 17:13:11 +0300

    added mapbox api key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
             scrollWheelZoom: false
         });
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-            mapboxToken: '<your MAPBOX API KEY HERE>',
+            mapboxToken: 'pk.eyJ1IjoibWFyaWFpb2FuYTg5IiwiYSI6ImNqa2UxdzN1ZTN3M3AzcG1xbzhmazhycjAifQ.3aI1wIXXIfF4CmpCibmT6w',
             maxZoom: 18,
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
                 '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -88,7 +88,7 @@ initMap = () => {
 
         updateRestaurants();
     }
-    /* window.initMap = () => {
+     window.initMap = () => {
       let loc = {
         lat: 40.722216,
         lng: -73.987501
@@ -99,7 +99,7 @@ initMap = () => {
         scrollwheel: false
       });
       updateRestaurants();
-    } */
+    } 
 
 /**
  * Update page and map for current restaurants.
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
                 scrollWheelZoom: false
             });
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-                mapboxToken: '<your MAPBOX API KEY HERE>',
+                mapboxToken: "https://maps.googleapis.com/maps/api/js?key=AIzaSyA_tKbW6A5pQ-eupxI56myUnHLqYCzOjKo&libraries=places&callback=initMap",
                 maxZoom: 18,
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
                     '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
